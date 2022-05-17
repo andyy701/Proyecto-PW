aef5599a2bed7fc6ea6358da2811fd8fd35dc23c
Diego Enrique Acuna Oyola <20122469@aloe.ulima.edu.pe>
2022-05-17 14:07:57 -0500

    desde la h6 hasta la h12

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+
+  // const agregar = document.getElementById('agregar')
+   const recordatorio = document.getElementById('recordatorio')
+  // const especialidad = document.getElementById('especialidad')
+  
+  // let dias = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo']
+  // let modalidad = ['Presencial','Online']
+  // let especialidades =['Ginecólogo','Pediatra','Dermatólogo','Traumatólogo y ortopedista','Gastroenterólogo','Cirujano general','Psiquiatra','Oftalmólogo','Neurólogo','Urólogo','Otorrino','Endocrinólogo','Cardiólogo','Neurocirujano','Cirujano Plástico','Internista','Neumólogo','Reumatólogo','Médico General','Cirujano Cardiovascular y Torácico','Dentista','Nefrólogo','Nutricionista','Oncólogo','Cirujano Maxilofacial','Hematólogo','Geriatra','Especialista en Medicina Física y Rehabilitación','Neumólogo Pediátrico','Alergista','Neonatólogo','Radiólogo','Anestesiólogo','Cirujano Pediátrico','Infectólogo','Especialista En Medicina Intensiva','Especialista En Administración De Salud','Neurofisiólogo Clínico','Patólogo Clínico','Especialista en Emergencias','Especialista En Medicina Estética','Quiropráctico','Fisioterapeuta','Terapeuta Complementario','Médico Legal','Médico Familiar','Genetista','Fonoaudiólogo','Epidemiólogo','Cirujano Vascular','Especialista En Medicina Nuclear','Médico Del Trabajo','Bioquímico','Médico Ocupacional','Especialista En Salud Pública','Homeópata','Farmacólogo','Médico Del Deporte','Podólogo','Especialista En Medicina Natural','Técnico En Laboratorio Clínico','Enfermero','Especialista En Radioterapia','Óptico']
+  
+  // for (let a=0; a<especialidades.length;a++) {
+  //   const option3 = document.createElement('option')
+  //   option3.setAttribute('value',especialidades[a])
+  //   option3.textContent = especialidades[a]
+
+  //   especialidad.appendChild(option3)
+  // }
+
+    //ERROR!
+  recordatorio.addEventListener('click', () =>{
+    alert("Recordatorio enviado.");
+  })
+
+//   agregar.addEventListener('click', () => {
+
+//     container = document.getElementById("container")
+
+//     select = document.createElement('select')
+//     container.appendChild(select)
+
+//     for (let i=0; i<7;i++) {
+//         option = document.createElement('option')
+//         option.setAttribute('value',dias[i])
+//         option.textContent = dias[i]
+//         select.appendChild(option)
+
+//     }
+
+//     time = document.createElement('input')
+//     time.setAttribute('type','time')
+//     container.appendChild(time)
+
+//     select2 = document.createElement('select')
+//     container.appendChild(select2)
+
+//     for (let j=0; j<2;j++) {
+//         option2 = document.createElement('option')
+//         option2.setAttribute('value',modalidad[j])
+//         option2.textContent = modalidad[j]
+//         select2.appendChild(option2)
+//     }
+
+//     eliminar = document.createElement('button')
+//     eliminar.textContent = 'Eliminar'
+//     container.appendChild(eliminar)
+
+//     eliminar.addEventListener('click', () => {
+
+//         select.remove();
+//         time.remove();
+//         select2.remove();
+//         eliminar.remove();
+        
+//     })
+    
+// })
+
+
+
+
+  
+  
